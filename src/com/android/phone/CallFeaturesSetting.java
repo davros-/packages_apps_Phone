149cf2c13b395f02b6763a8c4ce68551ce5c7512
buckmarble <forrestmarble@gmail.com>
2013-02-24 08:33:44 -0500

    fix missing }

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -1858,6 +1858,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         if (migrateVoicemailVibrationSettingsIfNeeded(prefs)) {
             mVoicemailNotificationVibrate.setChecked(prefs.getBoolean(
                     BUTTON_VOICEMAIL_NOTIFICATION_VIBRATE_KEY, false));
+        }
         if (mFlipAction != null) {
             updateFlipActionSummary(mFlipAction.getValue());
         }
